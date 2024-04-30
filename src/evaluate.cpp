080426b923c2ebc992b072a5e839f8f4e1442fc3
skystarspython <110183782+skystarspython@users.noreply.github.com>
2024-04-30 23:33:19 +0800

    test

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -103,6 +103,16 @@ namespace {
         {S(-582, -4894), S(2260, -2360), S(4002, -2435), S(4595, 1090), S(5389, 2949), S(9760, 3209), S(8500, 3453), S(11956, 6472), S(13619, 7657)}, // KNIGHT
         {S(1692, -2811), S(911, -1898), S(3017, -904), S(7134, 1537), S(9276, -1351)}, // BISHOP
     };
+    constexpr Score protectionBonus[2][PIECE_TYPE_NB] = {
+        {
+            S(1833, 112), S(1005, 1373), S(460, -327), S(-1001, -1037),
+            S(-2677, -321), S(-879, 5306), S(2328, 743), S(3864, 3564)
+        }, // ROOK
+        {
+            S(-841, -20), S(4270, 4745), S(-3281, -1955), S(1673, 3340),
+            S(-1139, 2037), S(-959, 991), S(-1884, 1464), S(-2588, 315)
+        } // KNIGHT
+    };
 #undef S
 
     // Evaluation class computes and stores attacks tables and other working data
@@ -134,6 +144,8 @@ namespace {
         Bitboard attackedBy2[COLOR_NB];
 
         Score mobility[COLOR_NB] = { SCORE_ZERO, SCORE_ZERO };
+
+        Score protection[COLOR_NB] = { SCORE_ZERO, SCORE_ZERO };
     };
 
 
@@ -186,8 +198,16 @@ namespace {
             attackedBy[Us][ALL_PIECES] |= b;
 
             int mob = popcount(b & ~attackedBy[Them][PAWN]);
-            if constexpr (Pt != PAWN)
-                mobility[Us] += mobilityBonus[Pt][mob];
+            mobility[Us] += mobilityBonus[Pt][mob];
+
+            if constexpr (Pt == KNIGHT || Pt == ROOK) {
+                Bitboard protectedBB = b & pos.pieces(Us) & (~attackedBy2[Them]) & (~attackedBy[Them][PAWN]);
+                while (protectedBB) {
+                    Square protectedSq = pop_lsb(protectedBB);
+                    PieceType protectedPt = type_of(pos.piece_on(protectedSq));
+                    protection[Us] += protectionBonus[Pt][protectedPt];
+                }
+            }
 
             if constexpr (Pt == CANNON) { // 炮的评估
                 int blocker = popcount(between_bb(s, ksq) & pos.pieces()) - 1;
@@ -206,6 +226,11 @@ namespace {
                 if (rank_of(s) == enemyBottom && !blocker && (ksq == SQ_E0 || ksq == SQ_E9) && (pos.pieces(Them) & enemyCenter)) { // 沉底炮
                     score += BottomCannon;
                 }
+            }
+            if constexpr (Pt == ROOK)
+            {
+                if (pos.is_on_semiopen_file(Us, s))
+                    score += RookOnOpenFile[pos.is_on_semiopen_file(Them, s)];
             }
         }
         return score;
@@ -300,6 +325,8 @@ namespace {
         score += threat<WHITE>() - threat<BLACK>();
 
         score += (mobility[WHITE] - mobility[BLACK]) / 100;
+
+        score += (protection[WHITE] - protection[BLACK]) / 100;
 
         if constexpr (T) {
             Trace::add(THREAT, threat<WHITE>(), threat<BLACK>());
