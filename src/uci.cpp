0de440f26b8f78498ac03187e08a1d075ea90468
xiangqishare <100093845+xiangqishare@users.noreply.github.com>
2024-05-01 22:04:39 +0200

    Update uci.cpp

--- src/uci.cpp
+++ src/uci.cpp
@@ -129,7 +129,7 @@ namespace {
     int nodeList[] = {100, 200, 300, 400, 600, 800, 1200, 1600, 2400, 3200, 4800, 6400, 9600, 12800, 19200, 25600, 38400, 51200, 76800, 102400};
 
     if (Options["Skill Level"] < 20)
-        if (pos.count<KNIGHT>() + pos.count<ROOK>() + pos.count<CANNON>() <= 3)
+        if (pos.count<KNIGHT>() + pos.count<ROOK>() + pos.count<CANNON>() <= 4)
             limits.nodes = nodeList[(int)Options["Skill Level"]] * 150 / 100;
         else
             limits.nodes = nodeList[(int)Options["Skill Level"]];
