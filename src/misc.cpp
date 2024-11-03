088b062a4e049583e055c3c13d05124b07a6ef13
New Horizons <110183782+skystarspython@users.noreply.github.com>
2024-11-03 23:24:49 +0800

    HCE

--- src/misc.cpp
+++ src/misc.cpp
@@ -149,7 +149,7 @@ string engine_info(bool to_uci) {
   string month, day, year;
   stringstream ss, date(__DATE__); // From compiler, format is "Sep 21 2008"
 
-  ss << "Pikafish " << Version << setfill('0');
+  ss << "PikafishHCE " << Version << setfill('0');
 
   if (Version.empty())
   {
